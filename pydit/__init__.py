ef0d9fb5368b38c8e213f99c0cd50b88720e0c09
Developer <jceresearch@users.noreply.github.com>
2022-02-20 10:51:29 +0000

    Working on new anonymisation and countif

--- pydit/__init__.py
+++ pydit/__init__.py
@@ -1,8 +1,15 @@
 """ when importing pydit this script is run and imports the entirety"""
-from .filemanager import *
-from .common import *
-from .modelling import *
-from .presenting import *
-from .profiling import *
-from .transform import *
-from .validate import *
+#from .filemanager import *
+#from .common import *
+#from .modelling import *
+#from .presenting import *
+#from .profiling import *
+#from .transform import *
+#from .validate import *
+import pydit.filemanager
+import pydit.common
+import pydit.transform
+import pydit.validate
+import pydit.modelling
+import pydit.presenting
+import pydit.profiling
