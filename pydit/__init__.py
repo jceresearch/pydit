ba6a4b1ec4db25a521a8e6b0d65b41ea1c352350
jceresearch <jceresearch@users.noreply.github.com>
2022-02-21 17:26:30 +0000

    Update __init__.py

--- pydit/__init__.py
+++ pydit/__init__.py
@@ -1,4 +1,5 @@
 """ when importing pydit this script is run and imports the entirety"""
+<<<<<<< HEAD
 from pydit import filemanager
 from pydit import common
 from pydit import modelling
@@ -6,3 +7,19 @@ from pydit import presenting
 from pydit import profiling
 from pydit import transform
 from pydit import validate
+=======
+#from .filemanager import *
+#from .common import *
+#from .modelling import *
+#from .presenting import *
+#from .profiling import *
+#from .transform import *
+#from .validate import *
+import pydit.filemanager
+import pydit.common
+import pydit.transform
+import pydit.validate
+import pydit.modelling
+import pydit.presenting
+import pydit.profiling
+>>>>>>> 4c6f771911b29eced7d7d40d3757c3d34bbe2c3c
