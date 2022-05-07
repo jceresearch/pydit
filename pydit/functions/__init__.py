4cfd0f11b7161c23a78785a11d39bb2e3474637b
jceresearch <jceresearch@users.noreply.github.com>
2022-05-07 18:02:04 +0100

    fixing code to remove dependencies on utils.py

--- pydit/functions/__init__.py
+++ pydit/functions/__init__.py
@@ -1,19 +1,4 @@
 # General Functions
-"""
-pyjanitor's general-purpose data cleaning functions.
-
-NOTE: Instructions for future contributors:
-
-1. Place the source code of the functions in a file named after the function.
-2. Place utility functions in the same file.
-3. If you use a utility function from another source file,
-please refactor it out to `janitor.functions.utils`.
-4. Import the function into this file so that it shows up in the top-level API.
-5. Sort the imports in alphabetical order.
-6. Try to group related functions together (e.g. see `convert_date.py`)
-7. Never import utils.
-"""
-
 
 from .create_calendar import create_calendar
 from .add_percentile import add_percentile
