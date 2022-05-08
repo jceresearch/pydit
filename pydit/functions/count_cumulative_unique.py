ce1a010e2568a2c4ade0592f33b1474da87fdd3c
Developer <jceresearch@users.noreply.github.com>
2022-05-08 07:14:30 +0100

    tweak function to avoid mutating input dataframe, also simplify documentation

--- pydit/functions/count_cumulative_unique.py
+++ pydit/functions/count_cumulative_unique.py
@@ -1,5 +1,5 @@
 """ Add a cumulative count of unique keys, mutates the df
-From Pyjanitor
+Adapted from similar function in Pyjanitor
 """
 
 from typing import Hashable
@@ -17,31 +17,29 @@ def count_cumulative_unique(
 ) -> pd.DataFrame:
     """Generates a running total of cumulative unique values in a given column.
 
+    :param df: A pandas dataframe.
+    :param column_name: Name of the column containing
+        values from which a running count of unique values
+        will be created.
+    :param dest_column_name: The name of the new column containing the
+        cumulative count of unique values that will be created.
+    :param case_sensitive: Whether or not uppercase and lowercase letters
+        will be considered equal (e.g., 'A' != 'a' if `True`).
+
+    :returns: A pandas DataFrame with a new column containing a cumulative
+    count of unique values from another column.
+    
+    
     Functional usage syntax:
 
     ```python
         import pandas as pd
-        import janitor as jn
+        import pydit
 
         df = pd.DataFrame(...)
 
-        df = jn.functions.count_cumulative_unique(
+        df = pydit.count_cumulative_unique(
             df=df,
-            column_name='animals',
-            dest_column_name='animals_unique_count',
-            case_sensitive=True
-        )
-    ```
-
-    Method chaining usage example:
-
-    ```python
-        import pandas as pd
-        import janitor
-
-        df = pd.DataFrame(...)
-
-        df = df.count_cumulative_unique(
             column_name='animals',
             dest_column_name='animals_unique_count',
             case_sensitive=True
@@ -54,26 +52,16 @@ def count_cumulative_unique(
     any letters will matter (i.e., `a != A`);
     otherwise, the case of any letters will not matter.
 
-    This method mutates the original DataFrame.
+    This method does NOT mutate the original DataFrame.
 
-    :param df: A pandas dataframe.
-    :param column_name: Name of the column containing
-        values from which a running count of unique values
-        will be created.
-    :param dest_column_name: The name of the new column containing the
-        cumulative count of unique values that will be created.
-    :param case_sensitive: Whether or not uppercase and lowercase letters
-        will be considered equal (e.g., 'A' != 'a' if `True`).
-
-    :returns: A pandas DataFrame with a new column containing a cumulative
-        count of unique values from another column.
+    
     """
-
+    df=df.copy()
     if not case_sensitive:
         # Make it so that the the same uppercase and lowercase
         # letter are treated as one unique value
         df[column_name] = df[column_name].astype(str).map(str.lower)
-
+    
     df[dest_column_name] = (
         (df[[column_name]].drop_duplicates().assign(dummyabcxyz=1).dummyabcxyz.cumsum())
         .reindex(df.index)
