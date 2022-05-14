a8b008c01857a78fff7e66b8f331fd7ddaaacdd0
jceresearch <jceresearch@users.noreply.github.com>
2022-05-14 20:18:50 +0100

    tweaking pylint to ignore # pylint: disable=redefined-outer-name

--- tests/test_coalesce_columns.py
+++ tests/test_coalesce_columns.py
@@ -5,10 +5,11 @@ import sys
 import numpy as np
 import pandas as pd
 import pytest
-from pandas.testing import assert_frame_equal, assert_series_equal
+from pandas.testing import assert_frame_equal
 
-
-# pylint: disable=import-error disable=wrong-import-position
+# pylint: disable=redefined-outer-name
+# pylint: disable=import-error 
+# pylint: disable=wrong-import-position
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pydit import coalesce_columns
 
