78e6c8968565c5f1fa7e79df20fd71c8fd89fe9f
jceresearch <jceresearch@users.noreply.github.com>
2022-05-15 10:50:05 +0100

    testing in fillna_smart

--- tests/test_fillna_smart.py
+++ tests/test_fillna_smart.py
@@ -25,9 +25,15 @@ def test_fillna_smart():
         "col5": [np.nan, "    ", "  \t", "   \n", "Value 5"],
     }
     df = pd.DataFrame(data=d)
-    df = fillna_smart(
+    result = fillna_smart(
         df, text_fillna="EMPTY", include_empty_string=True, include_spaces=True
     )
+
+    assert list(result["col2"]) == ["Value 1", "Value 2", "EMPTY", "EMPTY", "Value 5"]
+    assert list(result["col3"]) == [0.0, 2.0, 0.0, 4.0, 5.1]
+    assert list(result["col4"]) == [0, 2, 0, 4, 5]
+    assert list(result["col5"]) == ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "Value 5"]
+
     return df
 
 
