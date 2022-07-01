717e73af1f7b73f692b86d18fc8165b1869d52a4
jceresearch <jceresearch@users.noreply.github.com>
2022-07-01 07:59:16 +0000

    count_related() can do combined columns now

--- pydit/functions/counts.py
+++ pydit/functions/counts.py
@@ -6,23 +6,35 @@ from typing import Hashable
 import pandas as pd
 
 
-def count_related(df, col, column_name=None):
-    """Count the number of values from col
+def count_related(df, col, column_name=None, combined=False, inplace=False):
+    """Generates a column counting occurrence of values in a given column.
 
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Dataframe to be analyzed
-    col : str or list of str
-        Name of the column containing values to tally
-    column_name : str or list of str, optional, default None
-        Name of the columns to be created containing the count of values
-        If None, the column name will be "count_[col]".
+    If several columns provided, it will generate a column for each of them, but
+    if combined is True, it will generate a column counting unique
+    combinations of values in the columns.
 
-    Returns
-    -------
-    pd.DataFrame
-        New dataframe with a new column containing the count of values
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Dataframe to be analyzed
+        col : str or list of str
+            Name of the column containing values to tally
+        column_name : str or list of str, optional, default None
+            Name of the columns to be created containing the count of values
+            If None, the column name will be "count_[col]".
+        combined : bool, optional, default False
+            Whether or not compute the counts combining all the columns provided
+        inplace : bool, optional, default False
+            Whether or not to mutate the original DataFrame
+
+
+        Returns
+        -------
+        pd.DataFrame
+            New dataframe with a new column containing the count of values
+            If inplace is True, the original DataFrame is modified.
+
     """
     if not isinstance(df, pd.DataFrame):
         raise TypeError("Expecting a dataframe")
@@ -31,7 +43,6 @@ def count_related(df, col, column_name=None):
         flag_auto_name = True
     else:
         flag_auto_name = False
-
     if isinstance(col, str):
         cols_list = [col]
         if isinstance(column_name, str) and str.strip(column_name) != "":
@@ -42,7 +53,6 @@ def count_related(df, col, column_name=None):
             raise TypeError("column_name must be a string or list of strings")
         else:
             flag_auto_name = True  # we ignore the column_name
-
     elif isinstance(col, list):
         cols_list = col
         if isinstance(column_name, list):
@@ -52,26 +62,34 @@ def count_related(df, col, column_name=None):
             flag_auto_name = True  # ignore whatever we put there
     else:
         raise TypeError("Expecting a string or list of strings")
-
     for c in cols_list:
         if c not in df.columns:
             raise ValueError("Column not found in dataframe")
-    df = df.copy()
-    for i, c in enumerate(cols_list):
+    if not inplace:
+        df = df.copy()
 
-        def _get_counts(val):
-            if pd.isna(val):
-                return df[col].isna().sum()
-            else:
-                return len(df[df[col] == val])
-
-        count_list = [_get_counts(val) for index, val in enumerate(df[col])]
+    if combined:
+        s1 = df[cols_list].astype("str").T.agg("_".join)
+        df["combined_count_source"] = s1
+        count_summary = s1.value_counts(dropna=False)
+        count_list = [count_summary[val] for index, val in enumerate(s1)]
         if flag_auto_name:
-            cn = "count_" + c
+            cn = "count_combined"
         else:
-            cn = column_name[i]
+            cn = column_name[0]
         df[cn] = count_list
+    else:
+        for i, c in enumerate(cols_list):
+            count_summary = df[c].value_counts(dropna=False)
+            count_list = [count_summary[val] for index, val in enumerate(df[c])]
+            if flag_auto_name:
+                cn = "count_" + c
+            else:
+                cn = column_name[i]
+            df[cn] = count_list
 
+    if inplace:
+        return True
     return df
 
 
@@ -115,7 +133,6 @@ def count_cumulative_unique(
         # Make it so that the the same uppercase and lowercase
         # letter are treated as one unique value
         df[column_name] = df[column_name].astype(str).map(str.lower)
-
     df[dest_column_name] = (
         (df[[column_name]].drop_duplicates().assign(dummyabcxyz=1).dummyabcxyz.cumsum())
         .reindex(df.index)
--- tests/test_count_related.py
+++ tests/test_count_related.py
@@ -1,4 +1,5 @@
 """ pytest module for count_related function"""
+from math import comb
 import os
 import sys
 
@@ -16,25 +17,51 @@ def test_count_related():
     """test count related basic functionality"""
     df = pd.DataFrame(
         {
-            "class": ["bird", "bird", "bird", "mammal", "mammal"],
-            "max_speed": [389, 389, 0, 0, np.nan],
-            "country": ["UK", " ", "", "", np.nan],
-            "type": ["Falcon", "falcon", "parrot", "Lion", "Monkey"],
+            "class": ["bird", "bird", "bird", "mammal", "mammal", "insect"],
+            "max_speed": [389, 389, 0, 0, np.nan, np.nan],
+            "country": ["UK", " ", "", "", np.nan, np.nan],
+            "type": ["Falcon", "falcon", "parrot", "Lion", "Monkey", "Bee"],
+            "type_lower": ["falcon", "falcon", "parrot", "lion", "monkey", "bee"],
         }
     )
     res = count_related(df, "class", "count")
-    exp = [3, 3, 3, 2, 2]
+    exp = [3, 3, 3, 2, 2, 1]
     assert list(res["count"]) == exp
     res = count_related(df, "max_speed", "count_max_speed")
-    exp = [2, 2, 2, 2, 1]
+    exp = [2, 2, 2, 2, 2, 2]
     assert list(res["count_max_speed"]) == exp
     res = count_related(df, "country", "count")
-    exp = [1, 1, 2, 2, 1]
+    exp = [1, 1, 2, 2, 2, 2]
     assert list(res["count"]) == exp
     res = count_related(df, "type", "count")
-    exp = [1, 1, 1, 1, 1]
+    exp = [1, 1, 1, 1, 1, 1]
     assert list(res["count"]) == exp
+
+
+def test_count_related_combined():
+    """test combined count"""
+    df = pd.DataFrame(
+        {
+            "class": ["bird", "bird", "bird", "mammal", "mammal", "insect"],
+            "max_speed": [389, 389, 0, 0, np.nan, np.nan],
+            "country": ["UK", " ", "", "", np.nan, np.nan],
+            "type": ["Falcon", "falcon", "parrot", "Lion", "Monkey", "Bee"],
+            "type_lower": ["falcon", "falcon", "parrot", "lion", "monkey", "bee"],
+        }
+    )
+    res = count_related(df, ["class", "type"], "count", combined=True)
+    exp = [1, 1, 1, 1, 1, 1]
+    assert list(res["count_combined"]) == exp
+
+    res = count_related(df, ["class", "type_lower"], combined=True)
+    exp = [2, 2, 1, 1, 1, 1]
+    assert list(res["count_combined"]) == exp
+
+    res = count_related(df, ["max_speed", "country"], combined=True)
+    exp = [1, 1, 2, 2, 2, 2]
+    assert list(res["count_combined"]) == exp
 
 
 if __name__ == "__main__":
-    test_count_related()
+    # test_count_related_combined()
+    pass
