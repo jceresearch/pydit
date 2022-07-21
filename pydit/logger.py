9c36b9ca21369cca09f05c27c1767c2c01fc8a1f
jceresearch <jceresearch@users.noreply.github.com>
2022-07-21 19:23:48 +0000

    logger with colors for console and goes to sout

--- pydit/logger.py
+++ pydit/logger.py
@@ -1,6 +1,150 @@
 """ Module to provide support for logging with typical parameters """
 import logging
+import sys
 from logging.handlers import TimedRotatingFileHandler
+from typing import Optional, Dict
+
+# ANSI escape codes, picked from library Colorama to avoid dependency
+
+CSI = "\033["
+OSC = "\033]"
+BEL = "\a"
+
+
+def code_to_chars(code):
+    return CSI + str(code) + "m"
+
+
+def set_title(title):
+    return OSC + "2;" + title + BEL
+
+
+def clear_screen(mode=2):
+    return CSI + str(mode) + "J"
+
+
+def clear_line(mode=2):
+    return CSI + str(mode) + "K"
+
+
+class AnsiCodes(object):
+    def __init__(self):
+        # the subclasses declare class attributes which are numbers.
+        # Upon instantiation we define instance attributes, which are the same
+        # as the class attributes but wrapped with the ANSI escape sequence
+        for name in dir(self):
+            if not name.startswith("_"):
+                value = getattr(self, name)
+                setattr(self, name, code_to_chars(value))
+
+
+class AnsiCursor(object):
+    def UP(self, n=1):
+        return CSI + str(n) + "A"
+
+    def DOWN(self, n=1):
+        return CSI + str(n) + "B"
+
+    def FORWARD(self, n=1):
+        return CSI + str(n) + "C"
+
+    def BACK(self, n=1):
+        return CSI + str(n) + "D"
+
+    def POS(self, x=1, y=1):
+        return CSI + str(y) + ";" + str(x) + "H"
+
+
+class AnsiFore(AnsiCodes):
+    BLACK = 30
+    RED = 31
+    GREEN = 32
+    YELLOW = 33
+    BLUE = 34
+    MAGENTA = 35
+    CYAN = 36
+    WHITE = 37
+    RESET = 39
+
+    # These are fairly well supported, but not part of the standard.
+    LIGHTBLACK_EX = 90
+    LIGHTRED_EX = 91
+    LIGHTGREEN_EX = 92
+    LIGHTYELLOW_EX = 93
+    LIGHTBLUE_EX = 94
+    LIGHTMAGENTA_EX = 95
+    LIGHTCYAN_EX = 96
+    LIGHTWHITE_EX = 97
+
+
+class AnsiBack(AnsiCodes):
+    BLACK = 40
+    RED = 41
+    GREEN = 42
+    YELLOW = 43
+    BLUE = 44
+    MAGENTA = 45
+    CYAN = 46
+    WHITE = 47
+    RESET = 49
+
+    # These are fairly well supported, but not part of the standard.
+    LIGHTBLACK_EX = 100
+    LIGHTRED_EX = 101
+    LIGHTGREEN_EX = 102
+    LIGHTYELLOW_EX = 103
+    LIGHTBLUE_EX = 104
+    LIGHTMAGENTA_EX = 105
+    LIGHTCYAN_EX = 106
+    LIGHTWHITE_EX = 107
+
+
+class AnsiStyle(AnsiCodes):
+    BRIGHT = 1
+    DIM = 2
+    NORMAL = 22
+    RESET_ALL = 0
+
+
+Fore = AnsiFore()
+Back = AnsiBack()
+Style = AnsiStyle()
+Cursor = AnsiCursor()
+
+
+class ColoredFormatter(logging.Formatter):
+    """Colored log formatter."""
+
+    def __init__(
+        self, *args, colors: Optional[Dict[str, str]] = None, **kwargs
+    ) -> None:
+        """Initialize the formatter with specified format strings."""
+
+        super().__init__(*args, **kwargs)
+
+        self.colors = colors if colors else {}
+
+    def format(self, record) -> str:
+        """Format the specified record as text."""
+
+        record.color = self.colors.get(record.levelname, "")
+        record.reset = Style.RESET_ALL
+
+        return super().format(record)
+
+
+formatter_screen = ColoredFormatter(
+    "{asctime} - {color} {levelname:8} {reset} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    colors={
+        "DEBUG": Fore.CYAN,
+        "INFO": Fore.GREEN,
+        "WARNING": Fore.YELLOW + Back.BLACK,
+        "ERROR": Fore.RED + Back.BLACK,
+        "CRITICAL": Fore.RED + Back.WHITE + Style.BRIGHT,
+    },
+)
 
 
 def setup_logging(
@@ -38,12 +182,12 @@ def setup_logging(
     fh = TimedRotatingFileHandler(logfile, when="midnight", backupCount=7)
 
     fh.setLevel(level_file)
-    ch = logging.StreamHandler()
+    ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(level_screen)
     formatter = logging.Formatter(
         "%(asctime)s %(levelname)s - %(message)s", "%Y-%m-%d %H:%M"
     )
-    ch.setFormatter(formatter)
+    ch.setFormatter(formatter_screen)
     fh.setFormatter(formatter)
     log.addHandler(fh)
     log.addHandler(ch)
