925493a3dcbc87833828af074fc6413db535b59c
jceresearch <jceresearch@users.noreply.github.com>
2022-07-30 20:06:23 +0000

    Starting to work on charts functionality

--- pydit/functions/__init__.py
+++ pydit/functions/__init__.py
@@ -11,7 +11,7 @@ There are currently no exceptions to this design principle.
 
 """
 
-from .calendar import create_calendar
+from .calendar_table import create_calendar
 from .percentile import add_percentile
 from .profile_dataframe_statistics import profile_dataframe
 from .duplicates import check_duplicates
