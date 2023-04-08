76da4ef26e84e5135a8ccb99755cca937c5e3261
jceresearch <jceresearch@users.noreply.github.com>
2023-04-08 15:45:49 +0000

    playing with ideas

---
+++ scrapbook/awesome.py
@@ -0,0 +1,16 @@
+''' Module Description '''
+
+import pandas as pd
+import numpy as np
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
+from pandas.api.types import is_datetime64_any_dtype
+import logging
+import os
+import sys
+from datetime import datetime, date, timedelta
+logger = logging.getLogger(__name__)
+
+# pylint: disable=logging-not-lazy
+# pylint: disable=logging-fstring-interpolation
+# pylint: disable=import-error disable=wrong-import-positionheader
