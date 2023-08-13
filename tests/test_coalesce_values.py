12794c60244f98799fe6435b65cdf7cac6dd8525
jceresearch <jceresearch@users.noreply.github.com>
2023-08-13 21:12:52 +0000

    odd behaviour with index i fixed

--- tests/test_coalesce_values.py
+++ tests/test_coalesce_values.py
@@ -31,12 +31,12 @@ def test_coalesce_values():
         "f": ["a", "b", "c", "d", "e", "f", "g"],
         "g": ["a", "a", "a", "b", "b", "c", np.nan],
         "h": [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-        "i": [np.nan, np.nan, np.nan, "b", "b", "c", "d"],
+        "k": [np.nan, np.nan, np.nan, "b", "b", "c", "d"],
     }
     df = pd.DataFrame(data)
 
-    result = coalesce_values(df, "i", top_n_values_to_keep=2, dropna=False)
-    assert list(result["i_collapsed"]) == [
+    result = coalesce_values(df, "k", top_n_values_to_keep=2, dropna=False)
+    assert list(result["k_collapsed"]) == [
         "N/A",
         "N/A",
         "N/A",
