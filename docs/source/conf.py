4e5d18bfba01c2493d0044c5a68760897854ac30
jceresearch <jceresearch@users.noreply.github.com>
2023-08-26 15:20:25 +0000

    config tweaks for new version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ project = "pydit"
 author = "jceresearch"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.18"
+release = "0.1.01"
 
 
 # -- General configuration ---------------------------------------------------
