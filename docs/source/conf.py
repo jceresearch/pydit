22ac34195c7beb63444327e3f4ce97945e144575
jceresearch <jceresearch@users.noreply.github.com>
2023-10-04 09:36:54 +0000

    new version published

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ project = "pydit"
 author = "jceresearch"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.02"
+release = "0.1.03"
 
 
 # -- General configuration ---------------------------------------------------
